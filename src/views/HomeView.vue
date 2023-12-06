1c316139942f1af838f3d932b51913d963a78d5b
adrajagopal <adrajagopal@gmail.com>
2023-12-05 19:00:37 -0600

    Set up leafletJS map

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,34 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { onMounted } from "vue";
+
+onMounted(() => {
+	var map = L.map("map").setView([43.07485907699501, -89.38413717048415], 12);
+
+	L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+		maxZoom: 19,
+		attribution:
+			'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	}).addTo(map);
+
+	var greenOwl = L.marker([43.0924168968808, -89.35550384616374]).addTo(map);
+
+	var message = "Green Owl";
+
+	greenOwl.bindPopup(
+		`<h1 style='font-weight: 700;'>${message}</h1><p>no desserts for u, dirty binch ass</p>`,
+	);
+	// ideally you'd use the L.popup() factory to create popups
+});
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+	<main>
+		<div id="map"></div>
+	</main>
 </template>
+
+<style scoped>
+#map {
+	height: 400px;
+}
+</style>

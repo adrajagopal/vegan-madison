c74631eab4fcc6471cb82ffbd34b383364987a9c
adrajagopal <adrajagopal@gmail.com>
2023-12-05 23:56:58 -0600

    Create Add Restaurant page
    same

---
+++ src/views/AddRestaurantView.vue
@@ -0,0 +1,19 @@
+<script setup>
+import AddRestaurantForm from "@/components/AddRestaurantForm.vue";
+</script>
+
+<template>
+	<inner-column>
+		<AddRestaurantForm />
+	</inner-column>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+	.about {
+		min-height: 100vh;
+		display: flex;
+		align-items: center;
+	}
+}
+</style>

c4ace5dd6c7e29a317343334d289d2cdecb4c924
adrajagopal <adrajagopal@gmail.com>
2023-12-05 22:45:25 -0600

    add eslint - but leave commented out

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,34 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+// import eslint from "@rollup/plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+	plugins: [
+		vue({
+			template: {
+				compilerOptions: {
+					isCustomElement: function (element) {
+						const customElements = ["inner-column"];
+
+						return customElements.includes(element);
+					},
+				},
+			},
+		}),
+		// {
+		// 	...eslint({
+		// 		include: "src/**/*.+{js}",
+		// 	}),
+		// 	enforce: "pre",
+		// 	apply: "build",
+		// },
+	],
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
+});

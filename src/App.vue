d1f6bfc1ce5b24692ea31af5ab3989baa9f707b3
adrajagopal <adrajagopal@gmail.com>
2023-12-11 11:20:23 -0600

    move header into component file

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,15 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import SiteHeader from "@/components/SiteHeader.vue";
 </script>
 
 <template>
 	<header>
 		<inner-column>
-			<div class="site-header">
-				<picture class="logo">
-					<img alt="VM logo" src="@/assets/logo.svg" />
-				</picture>
-
-				<div class="wrapper">
-					<nav>
-						<RouterLink to="/">Home</RouterLink>
-						<RouterLink to="/add-restaurant">Add restaurant</RouterLink>
-					</nav>
-				</div>
-			</div>
+			<SiteHeader />
 		</inner-column>
 	</header>
 
 	<RouterView />
 </template>
 
-<style scoped>
-.site-header {
-	display: flex;
-	justify-content: space-between;
-
-	& nav {
-		display: flex;
-		gap: 10px;
-	}
-}
-
-.logo {
-	width: 100px;
-}
-</style>
+<style scoped></style>
---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+<template>
+	<div class="site-header">
+		<picture class="logo">
+			<img alt="VM logo" src="@/assets/logo.svg" />
+		</picture>
+
+		<div class="wrapper">
+			<nav>
+				<RouterLink to="/">Home</RouterLink>
+				<RouterLink to="/add-restaurant">Add restaurant</RouterLink>
+			</nav>
+		</div>
+	</div>
+</template>
+<style scoped>
+.site-header {
+	display: flex;
+	justify-content: space-between;
+
+	& nav {
+		display: flex;
+		gap: 10px;
+	}
+}
+
+.logo {
+	width: 100px;
+}
+</style>
